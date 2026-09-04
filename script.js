dbdd7bd708c313de82a1e5ce43286f64505f6f11
Nero-http <shs2211235.dy@csav.edu.ph>
2026-09-04 16:31:22 +0800

    querySelector

--- script.js
+++ script.js
@@ -20,3 +20,12 @@ function greet(greetings, names = []) {
     return `${greetings} sa enyu, ${names}`;
 }
 console.log ( greet("Mayong aga", ["Anghel","Cholo","Toto","Nero."]) );
+
+const heading = document.querySelector("h1");
+console.log (heading);
+
+const contactHeading = document.querySelector ("#contact h2");
+console.log (contactHeading);
+
+const sectionHeading = document.querySelector ("#services h2");
+console.log (sectionHeading);
