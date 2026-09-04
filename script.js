224976aac638b9b2a926e3f2d6a02f6739561562
Nero-http <shs2211235.dy@csav.edu.ph>
2026-09-04 14:37:57 +0800

    variables (let and const)

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 console.log ("Hello World");
 console.log ("From script.js file")
+
+const myName = "Anghel";
+let number = "09056667962";
+let address = "Villa Miranda";
+let sixsivin = 67;
+let age = 21;
+
+console.log (`Name: ${myName}`);
+console.log (`Age: ${age}`);
+console.log (`Number: ${number}`);
+console.log (`Address: ${address}`);
+console.log (`${sixsivin}`);
